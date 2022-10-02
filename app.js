e2095cfcd5e0e8189012ff848c0d9bef12a37b08
fanisvl <fanis.vlahogiannis@gmail.com>
2022-10-02 12:26:45 +0300

    Create a 16x16 + 1 (bug-block) grid

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ box.classList.add('box');
 gridContainer.appendChild(box);
 
 function row(rowNumber) {
-    for (let box = 0; box <= 16; box++) {
+    for (let box = 1; box <= 16; box++) {
         let box = document.createElement('div');
         box.classList.add('box');
         gridContainer.appendChild(box);
@@ -12,7 +12,7 @@ function row(rowNumber) {
 }
 
 function grid() {
-    for (let rowNum = 0; rowNum <= 16; rowNum++) {
+    for (let rowNum = 1; rowNum <= 16; rowNum++) {
         row(rowNum);
     }
 }
