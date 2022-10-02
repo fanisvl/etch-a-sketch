f6d9dcd85aaa23fab725f6fe636ecd9df9a67006
fanisvl <fanis.vlahogiannis@gmail.com>
2022-10-02 20:14:14 +0300

    Reset button clears canvas

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 let gridContainer = document.querySelector('#grid-container')
 let resetBtn = document.querySelector('#resetBtn');
-resetBtn.addEventListener('click', reset);
-let box = document.createElement('div');
 
 function row() {
     for (i = 1; i <= 16; i++) {
+        let box = document.createElement('div');
         box.classList.add('box');
         gridContainer.appendChild(box);
         box.addEventListener('mouseover', () => {
@@ -13,13 +12,21 @@ function row() {
     }
 }
 
-function grid() {
+function grid(rowNum) {
     for (rowNum = 1; rowNum <= 16; rowNum++) {
         row(rowNum);
     }
 }
 
+resetBtn.addEventListener('click', () => {
+    reset();
+});
+
 function reset() {
+    let boxes = document.querySelectorAll('.box');
+    for(let i of boxes) {
+        i.classList.remove('addColor');
+    }
 }
 
 grid();
