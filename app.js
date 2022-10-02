16ccbfac8fb8bc3c627435e81dd3c3dbf9ce63f2
fanisvl <fanis.vlahogiannis@gmail.com>
2022-10-02 18:06:47 +0300

    change for var name to i

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 let gridContainer = document.querySelector('#grid-container')
 let resetBtn = document.querySelector('#resetBtn');
 resetBtn.addEventListener('click', reset);
+let box = document.createElement('div');
 
-function row(box) {
-    for (box = 1; box <= 16; box++) {
-        let box = document.createElement('div');
+function row() {
+    for (i = 1; i <= 16; i++) {
         box.classList.add('box');
         gridContainer.appendChild(box);
         box.addEventListener('mouseover', () => {
@@ -13,14 +13,13 @@ function row(box) {
     }
 }
 
-function grid(rowNum) {
+function grid() {
     for (rowNum = 1; rowNum <= 16; rowNum++) {
         row(rowNum);
     }
 }
 
 function reset() {
-
 }
 
 grid();
