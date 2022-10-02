f840a83fc8b6b31310e9aa2f619412de40c684cb
fanisvl <fanis.vlahogiannis@gmail.com>
2022-10-02 11:41:07 +0300

    Create a row function, add flex to grid-container

--- app.js
+++ app.js
@@ -2,3 +2,13 @@ let gridContainer = document.querySelector('#grid-container')
 let box = document.createElement('div')
 box.classList.add('box');
 gridContainer.appendChild(box);
+
+function row() {
+    for (let i = 0; i <= 16; i++) {
+        let i = document.createElement('div');
+        i.classList.add('box');
+        gridContainer.appendChild(i);
+    }
+}
+
+row();
